953305a4e7a6eb9e9ba6a881cb9820c5d3e1ff48
Marios Choudary <marios.choudary@cs.pub.ro>
2018-01-13 16:26:48 +0200

    added some debug messages

--- smkex/libsmkex.c
+++ smkex/libsmkex.c
@@ -451,6 +451,10 @@ EC_POINT* __recv_remote_key(int sockfd, EC_KEY* key, int ids) {
         fprintf(stderr, "Error: Could not get remote public key.\n");
         goto err_recv_remotekey;
     }
+#if DEBUG
+    fprintf(stderr, "Remote key received on socket %d, subflow %d\n",
+        sockfd, ids);
+#endif
 
     // Allocate new point for remote public key
     const EC_GROUP* ec_group = EC_KEY_get0_group(key);
@@ -520,12 +524,18 @@ int __recv_dummy(int sockfd) {
     }
 
     // Get dummy packet on whatever channel is available
+#if DEBUG
+    fprintf(stderr, "libsmkex: in recv_dummy, before smkex_pkt_rcv\n");
+#endif
     ppkt->recv = original_recv;
     int rc = smkex_pkt_recv(ppkt, sockfd, 0);
     if (rc <= 0) {
         fprintf(stderr, "Error: Could not get dummy packet.\n");
         return -1;
     }
+#if DEBUG
+    fprintf(stderr, "libsmkex: in recv_dummy, after smkex_pkt_rcv\n");
+#endif
 
     // Discard dummy packet
     smkex_pkt_free(ppkt);
@@ -573,11 +583,17 @@ int connect(int sockfd, const struct sockaddr* address, socklen_t address_len) {
     }
 
     // Send dummy packet to force creating two subflows
+#if DEBUG
+    fprintf(stderr, "libsmkex: sending dummy packet to force creating two subflows\n");
+#endif
     rc = __send_dummy(sockfd);
     if (rc < 0) {
         fprintf(stderr, "Error: Could not send dummy packet.\n");
         return -1;
     }
+#if DEBUG
+    fprintf(stderr, "libsmkex: dummy packet sent\n");
+#endif
 
     // Block while waiting for slave subflows to be ready
     int slave_count = 2;
@@ -831,6 +847,9 @@ int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) {
     *addrlen = 0;
     int accepted_fd = original_accept(sockfd, addr, addrlen);
     if (accepted_fd >= 0) {
+#if DEBUG
+        fprintf(stderr, "libsmkex: accepted new connection, fd=%d\n", accepted_fd);
+#endif
         mp_sockets[accepted_fd].used = 1;
         mp_sockets[accepted_fd].recv_buffer = NULL;
         mp_sockets[accepted_fd].recv_buffer_cursor = NULL;
@@ -850,12 +869,18 @@ int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) {
     }
 
     // Receive dummy packet to force creating two subflows
+#if DEBUG
+    fprintf(stderr, "libsmkex: receiving dummy packet to force creating two subflows\n");
+#endif
     int rc = __recv_dummy(accepted_fd);
     if (rc < 0) {
         fprintf(stderr, "Error: Could not receive dummy packet.\n");
         mp_sockets[accepted_fd].used = 0;
         return -1;
     }
+#if DEBUG
+    fprintf(stderr, "libsmkex: dummy packet received\n");
+#endif
 
     // Block while waiting for slave subflows to be ready
     int slave_count = 2;
